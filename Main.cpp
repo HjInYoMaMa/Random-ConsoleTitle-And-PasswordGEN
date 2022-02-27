9d635c0b5625f4e253b65897cdd670e2592b521b
hj <88996806+HjInYoMaMa@users.noreply.github.com>
2022-02-27 02:05:14 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,12 +7,10 @@
 #include<ctime>
 using namespace std;
 
-//random title generate code:
 static const char rnum[] = "0123456789" "qwertyuiopasdfghjklzxcvbnm" "QWERTYUIOPASDFGHJKLZXCVBNM";
 int staticLength = sizeof(rnum) - 1;
 std::string RandomTitle;
 
-//random password generate code:
 const char alphanum[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int string_length = sizeof(alphanum) - 1;
 
